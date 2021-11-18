abb3c91d2153a8b6e7ff4c449132343699fb95c6
박병관 <85085375+codingbotPark@users.noreply.github.com>
2021-11-18 12:24:44 +0900

    make [login input type changer]
    commit before making [input type changer] in same box,input tag

--- loginpage/ducami-login.js
+++ loginpage/ducami-login.js
@@ -61,6 +61,8 @@ function getEvent(){
     for(let i = 0;i<joinarea.length;i++){
         joinarea[i].addEventListener("mouseover",showJoin);
     }
+
+    
     loginComment.addEventListener("click",clickLogin)
     joinComment.addEventListener("click",clickJoin)
 }
@@ -75,25 +77,60 @@ init();
 // 비밀번호 보이게, 안보이게
 const passwordInput =document.querySelector("#ducami-login-login-password");
 const showPassword = document.querySelector(".ducami-login-login-eye");
-const passwordImg = document.querySelector(".ucami-login-login-eyeImg");
+const passwordImg = document.querySelector(".ducami-login-login-eyeImg");
+
 showPassword.addEventListener("click", changeInputState);
+// showPassword.onclick = changeInputState;
 
 function changeInputState(){
     // 글씨의 타입을 바꿔줌, 사진변경
     if (passwordInput.getAttribute('type') == 'password'){
         passwordInput.setAttribute('type', 'text');
+        passwordImg.src = "../imgs/eye.png";
+        //스타일 변경
+        passwordImg.style.height = "115%";
+        passwordImg.style.weight = "115%";
     }
     else{ // text -> password = closeeye
         passwordInput.setAttribute('type','password');
         // 현제를 기준으로 파일 위치를 설정해서 파일이 변경되면 변경해야됨!
-        
+        passwordImg.src = "../imgs/closeeye.png";
+        passwordImg.style.height = "70%";
+        passwordImg.style.weight = "70%";
+        //스타일 변경
     }
 }
 
-// console.log(showPassword.children)
+
+// // login과 join시 enter키 입력시 바로 로그인
+// const loginInputs= document.querySelectorAll(".ducami-login-login-input input");
+// const joinInputs= document.querySelectorAll(".ducami-login-join-input input");
+// const certifiedInput = document.querySelector("#ducami-login-join-certified");
 
 
-// login과 join시 enter키 입력시 바로 로그인
+// // login input들 enter 받기
+// function makeLogin(){
+//     if (this.keyCode == 13){
+//         console.log("됨");
+//     }
+//     console.log("hi");
+// }
 
+// for (let i = 0;i<loginInputs.length;i++){
+//     loginInputs[i].addEventListener("onkeyup",makeLogin);
+// }
+// // join input들 enter 받기
+
+// //인증은 enter일 때 인증되도록 하기
+
+
+
+// 비밀번호 유효성 검사
+
+
+
+
+
+// 비밀번호 검사
 
 
