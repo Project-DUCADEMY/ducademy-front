2ed987549a482e82706c8fe507dac11e5952f37d
박병관 <85085375+codingbotPark@users.noreply.github.com>
2021-11-21 22:07:57 +0900

    make [showPassword] complete, start [checking inputs values]

---
+++ loginpage/ducami-login-checkJoin.js
@@ -0,0 +1,11 @@
+// 비교 후 형식에 맞으면 1, 안 맞으면 0
+let joinName = 0;
+let joinBirthday =0;
+let joinId =0;
+let joinNumber =0;
+let joinPassWord = 0;
+
+// 전체적인 것들이 형식에 맞는지 비교
+function checkAllJoin(){
+    
+}
---
+++ loginpage/ducami-login-checkLogin.js
@@ -0,0 +1,7 @@
+// 비교 후 형식에 맞으면 1, 안 맞으면 0
+let loginId =0;
+let loginPassword =0;
+
+function checkAllLogin(){
+    
+}
