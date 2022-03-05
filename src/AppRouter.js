6147afd1fceab07d400eaa94d5960a0e36633cde
건호 김 <qfourca3305@gmail.com>
2022-03-05 20:13:43 +0900

    [ADD] rtClass

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { atom, useRecoilState, useRecoilValue } from "recoil";
 import { useEffect } from "react"
 import axios from "axios"
+
 import Sidebar from './components/Sidebar/Sidebar.js';
 import Testpage from './components/Testpage.js'
-import Signpage from './components/signpage/Signpage.js';
-import Debuggerpage from './components/debuggerpage/Debugger.js';
+import Signpage from './components/Sign/Signpage.js';
+import Debuggerpage from './components/Debugger/Debugger.js';
 import Calendar from './components/Calendar/Calendar.js'
 import Test2 from './components/Loadingpage.js'
 import Errorpage from './components/Errorpage.js';
+import RtClass from './components/rtClass/rtCLass.js'
+
 import User from './global_helper/user'
 import Loading from './global_helper/loading'
 import ErrorRecoil from './global_helper/error'
@@ -21,6 +24,7 @@ const AppRouter = () => {
         console.log('test')
         axios.post('/ssr/userinfo')
         .then(response => {
+            setLoading(false);
             setUserinfo({
                 isAuthenticated: true,
                 userPage: response.data 
@@ -49,6 +53,7 @@ const AppRouter = () => {
                     </Route>
                     <Route path='/debugger'> <Debuggerpage/> </Route>
                     <Route path='/calendar'> <Calendar/> </Route>
+                    <Route path='/rtClass'> <RtClass/> </Route>
                     <Route path='/test2'> <Test2/> </Route>
                 </BrowserRouter>
             )
