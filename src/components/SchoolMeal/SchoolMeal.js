026c718e17276d90734dc114c2fb3365c9ea4753
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-03-12 22:00:24 +0900

    .

--- src/components/SchoolMeal/SchoolMeal.js
+++ src/components/SchoolMeal/SchoolMeal.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { niesAPIkey } from '../config/config.js'
 import style from '../../assets/css/SchoolMeal.module.css'
+import SchoolInfo from '../../assets/dummy/School'
 
 let Main = styled.div`
 `
@@ -10,6 +11,10 @@ let Main = styled.div`
 function SchoolMeal() {
     let [getSchoolMeal, setSchoolMeal] = useState([])
     let date = '20220310'
+    let STYLE = undefined
+    let timeStyle = [style.breakfast, style.lunch, style.dinner]
+    let timeName = ['아침', '점심', '저녁']
+    let idx = -1
     let menu = ''
     useEffect(() => {
         axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${niesAPIkey}&Type=json&ATPT_OFCDC_SC_CODE=${SchoolInfo.OfficeOfEduCode}&SD_SCHUL_CODE=${SchoolInfo.SchoolCode}&MLSV_YMD=${date}`)
@@ -29,9 +34,17 @@ function SchoolMeal() {
             <div className={style.container}>
             {
                 getSchoolMeal.map(element => {
+                    idx += 1
                     menu = element.DDISH_NM
-                    menu = menu.replaceAll('*', '').replaceAll('<br/>', '').replaceAll('\/[1-9.]+\/', ', ')
-                    return <span>{menu}</span>
+                    menu = menu.replaceAll('*', '').replaceAll(/[0-9.]+/g, '').replaceAll('<br/>', ', ')
+                    return (
+                        <div className={style.timeBlock}>
+                            <div className={style.menuFront}>
+                                <span className={timeStyle[idx]}>{timeName[idx]}</span>
+                            </div>
+                            <span className={style.menuBack}>{menu}</span>
+                        </div>
+                    )
                 })
             }
             </div>
