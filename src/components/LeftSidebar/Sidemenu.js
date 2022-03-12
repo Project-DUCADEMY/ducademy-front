625f0493219599d11a0d35cbc4ef797e40cc217e
건호 김 <qfourca3305@gmail.com>
2022-03-12 15:03:34 +0900

    Fix sidebar code design

--- src/components/LeftSidebar/Sidemenu.js
+++ src/components/LeftSidebar/Sidemenu.js
@@ -1,53 +1,42 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import { DisplayText, HideText } from './HideText';
-import { useRecoilState } from 'recoil';
-import { sideBarState } from './Sidebar';
 const Main = styled.div`
     transition-duration: 0.2s;
+    border-radius: 10px;
     height: 70px;
-    width: ${props => props.isOver ? '180%' : '60%'};
-    margin: 10px 20px 10px 20px;
+    width: ${props => props.isOver ? '250%' : '100%'};
+    float: left;
+    position: relative;
+    margin-left: ${props => props.isOver ? '25%' : '10%'};
+    margin-top: 10px;
     border-radius: 10px;
     :hover {
         background-color: rgba(200, 220, 200, 0.6);
         cursor: pointer;
     }
-    //text-align: right;
     z-index: 99;
 `
 const Content = styled.div`
-    /* float: right;
-    position: relative;
-    vertical-align: middle;
-    left: -8px;
-    transform:translateY(-50%);
-    top: 50%; */
-
-    //left: ${props => props.isOver ? '80%' : '0px'};
     position: ${props => props.isOver ? 'relative' : 'absolute'};
     transition: 0.2s;
     float: right;
     top: 23px;
-    right: 8px;
-    //bottom: ${props => props.isOver ? '-20px' : '20px'};
+    right: 10px;
     opacity: ${props => props.isOver ? '1' : '0'};
     overflow: hidden;
     display: ${props => props.isOver ? '0' : 'none'};
-    //display: none;
 `
 
 const Icon = styled.img`
-    height: 40px;
+    height: 70%;
     width: auto;
     position: relative;
-    left: 10px;
+    left: 7px;
     top: 50%;
     transform:translateY(-50%);
 `
 function Sidemenu (props) {
-    let [open, setOpen] = useRecoilState(sideBarState)
     const [getOver, setOver] = useState(false)
     return (
         <Link to={props.info.link}>
--- src/components/LeftSidebar/Userinfo.js
+++ src/components/LeftSidebar/Userinfo.js
@@ -1,29 +1,25 @@
 import { useRecoilValue } from 'recoil';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import User from '../../global_helper/user'
-import { DisplayText, HideText } from './HideText';
-import { sideBarState } from './Sidebar';
+
 const Main = styled.div`
-    position: relative;
+    height: 10%;
     width: auto;
-    height: 11%;
-    padding: 20px;
-    padding-bottom: 0px;
+    display: flex;
+    margin-top: 20px
 `
 const IdPhoto = styled.img`
-    width: 75px;
-    height: 75px;
+    margin: 0 auto;
+    width: 70px;
+    height: 70px;
     object-fit: cover;
     border-radius: 40%;
-    position: relative;
-    left: ${props=>props.open ? "0px" : "-7px"};
     transition : 0.4s;
 `
 const Infos = styled.div`
     position: absolute;
     right: 40px;
     bottom: 45px;
-    animation: ${props => props.open ? DisplayText : HideText};
     animation-duration: 0.4s;
     animation-fill-mode: forwards;
 `
@@ -41,19 +37,17 @@ const errorImgLink = (e) => {
     e.target.src = '/img/default.jpeg'
 }
 function Userinfo(props) {
-    let open = useRecoilValue(sideBarState)
     let userinfo = useRecoilValue(User)
     return (
         <Main>
             <IdPhoto 
                 src={ userinfo.userPage.photoLink } 
                 onError={ errorImgLink }
-                open = { open }
             />
-            <Infos open = { open } >
+            {/* <Infos>
                 <Name>{ userinfo.userPage.name }</Name>
                 <Role>{ userinfo.userPage.role }</Role>
-            </Infos>
+            </Infos> */}
         </Main>
     )
 }
